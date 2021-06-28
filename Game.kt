d644fee24dbdefc5a6846654142370236e980d5b
rain <chenu9999@gmail.com>
2021-06-28 13:14:30 +0800

    c13-1定義主建構函數

---
+++ Game.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val player = Player()
+    player.castFireball(5)
+
+    //光環
+    //player.auraColor() 用不到
+
+    //玩家狀態
+    printPlayerStatus(player)
+}
+
+private fun printPlayerStatus(player: Player) {
+    val statusFormatString = "(血量:${player.healthPoints}) (光環顏色:${player.auraColor()}) " +
+            "(運氣:${if (player.isBlessed) "好" else "壞"}) -> ${player.name} ${player.formatHealthStatus()}"
+    println(statusFormatString)
+}
+
+/*private fun drink(glass: Int) {
+    val drunkenness = when (glass) {
+        0 -> "清醒"
+        in 1..10 -> "微醺"
+        in 11..20 -> "微醉"
+        in 21..30 -> "醉了"
+        in 31..40 -> "大醉"
+        in 41..50 -> "爛醉如泥"
+        else -> "沒意識"
+    }
+    println("酒醉狀態: $drunkenness")
+}*/
---
+++ Player.kt
@@ -0,0 +1,43 @@
+class Player(_name: String,
+             _healthPoints: Int,
+             _isBlessed: Boolean,
+             _isImmortal: Boolean) {
+    var name = _name
+        get() = field.capitalize()
+        private set(value) {
+            field = value.trim()
+        }
+
+    var healthPoints = _healthPoints
+    var isBlessed = _isBlessed
+    private val isImmortal = _isImmortal
+
+    fun auraColor(): String {
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+        val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
+        val auraColor = if (auraVisible) when (karma) {
+            in 16..20 -> "綠色光環"
+            in 11..15 -> "紫色光環"
+            in 6..10 -> "橘黃色光環"
+            in 0..5 -> "紅色光環"
+            else -> "無光環"
+        } else "無光環"
+        return auraColor
+    }
+
+    fun formatHealthStatus() =
+        when (healthPoints) {
+            100 -> "狀態極佳"
+            in 90..99 -> "有一些小擦傷"
+            in 75..89 -> if (isBlessed) {
+                "雖有一些傷口，但恢復很快"
+            } else {
+                "有一些傷口"
+            }
+            in 15..74 -> "嚴重受傷"
+            else -> "狀態不妙"
+        }
+
+    fun castFireball(numFireballs: Int = 2) =
+        println("$numFireballs" + "杯Fireball突然出現")
+}
